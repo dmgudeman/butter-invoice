9dc1cd6f7ea41ec4e342e99be4aa0eb1ae498334
davidmgudeman <davidmgudeman@gmail.com>
2017-02-09 15:00:03 -0800

    using fxlayout - working on getting dual boxes to populate each row otherwise compiles well

--- src/app/company/company.service.ts
+++ src/app/company/company.service.ts
@@ -1,12 +1,14 @@
 import {Company} from './company';
+import { Injectable } from '@angular/core';
 
+@Injectable()
 export class CompanyService {
     companies: Company[] = [
         {name: "Exxon", color: "green", hourly: 250}, 
         {name: "Texaco", color: "blue", hourly: 225}, 
-        {name: "Sinclair",color: "brown", hourly: 200}
+        {name: "Sinclair",color: "brown", hourly: 200},
+        {name: "Shell",color: "red", hourly: 200}
         ];
-
     
    getCompanies(): Promise<Company[]> {
       return Promise.resolve(this.companies);
